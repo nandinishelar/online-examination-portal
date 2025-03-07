618bea3c5713b705dbe45fb27ea3d353994160f5
Nandini Santosh Shelar <99242347+nandinishelar@users.noreply.github.com>
2025-03-07 10:21:32 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,18 @@
+// function displayDate() {
+	// function body
+   
+//     var today = new Date()
+//     var curHr = today.getHours()
+//     document.getElementById("tm").innerHTML = curHr + ":" + today.getMinutes() + ":" + today.getSeconds();
+   
+//     document.getElementById("btn").innerHTML = "Current time is !!!!";
+  
+//     if (curHr < 12) {
+//         document.getElementById("image").src = "good morning.webp";
+//         document.getElementById("wish").innerHTML = "Good Morning";
+//     }
+//     else if(curHr<=16 && curHr >= 12) {
+//         document.getElementById("Body").style.backgroundImage = "url('sun.jpg')";
+//         document.getElementById("wish").innerHTML = "Good Afternoon";
+//     }
+// }
